529bc331411b61189effda07e191bbcf624f9b6a
2021302 <2021302@B213-11>
2022-02-03 08:49:10 +0100

    02.04 Add project files.

---
+++ Szamologep/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamologep
+{
+    class Program
+    {
+        static double Szamologep(string num1, string num2, string op)
+        {
+            return Convert.ToDouble(new DataTable().Compute(num1 + op + num2, null));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az első számot");
+            string x = Console.ReadLine();
+
+            Console.WriteLine("Adja meg a műveletet");
+            string o = Console.ReadLine();
+
+            Console.WriteLine("Adja meg a második számot");
+            string y = Console.ReadLine();
+
+            Console.WriteLine(Szamologep(x, y, o));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Szorzotabla/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szorzotabla
+{
+    class Program
+    {
+        static int[] Szorzotabla(int num1, int num2)
+        {
+            int[] szorzotabla = new int[num2];
+
+            for (int i = 0; i < num2; i++)
+            {
+                szorzotabla[i] = (i + 1) * num1;
+            }
+            return szorzotabla;
+        }
+        static void Main(string[] args)
+        {
+            foreach (var item in Szorzotabla(9, 12))
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
