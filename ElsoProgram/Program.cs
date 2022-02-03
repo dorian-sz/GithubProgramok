959825ea35dc70ef47047ca4453eb25409fa7d8f
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-02-03 10:00:34 +0100

    1

---
+++ ElsoProgram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElsoProgram
+{
+    class Program
+    {  
+        public int Szorzo(int num1, int num2)
+        {
+            int[] szorzotabla = new int[num2];
+
+            for (int i = 1; i < num2; i++)
+            {
+                
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+
+    }
+}
