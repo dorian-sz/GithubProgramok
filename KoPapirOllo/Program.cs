3d02e91b1e0dfbacfe8f5f2458d568573ff0933e
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-02-04 11:40:28 +0100

    02.04

---
+++ KoPapirOllo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoPapirOllo
+{
+    class Player
+    {
+        public string Choice;
+
+        static 
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player jatekos = new Player();
+            jatekos.Choice = Console.ReadLine();
+
+            Console.ReadLine();
+        }
+    }
+}
