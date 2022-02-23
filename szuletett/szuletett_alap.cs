6bc463dcdbcc515ee7a674fbf8fd294d6c98d6a6
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-02-23 10:00:55 +0100

    harmadik feladat

---
+++ szuletett/szuletett_alap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Szuletett
+{
+	String nev;
+	String foglalkozas;
+	String neme;
+	int szama;
+
+	public Szuletett(String nev, String foglalkozas, String neme)
+	{
+		this.nev = nev;
+		this.foglalkozas = foglalkozas;
+		this.neme = neme;
+		this.szama = 0;
+	}
+	public string Nem()
+    {
+        if (neme == "f")
+        {
+			return "Férfi";
+        }
+        else
+        {
+			return "Nő";
+        }
+    }
+	public int Szerencseszam()
+    {
+		Random rnd = new Random();
+		szama = rnd.Next();
+		return szama
+	}
+
+
+}
