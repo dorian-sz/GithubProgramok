90dcb1cf6f959175fd3fd76d0b7e72ea5e009fd4
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-02-23 10:50:42 +0100

    dolgozat feladat

---
+++ oszthato5el/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oszthato5el
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bekert = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < bekert; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Osztható 5-el. A szám: {0}", i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
