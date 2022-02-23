1f4c2c5de53c44c0e466d3532f28d7868309ab00
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-02-23 10:47:53 +0100

    dolgozat feladat

---
+++ 0igbeker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0igbeker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var szamok = new List<int>();
+            Console.WriteLine("Adjon meg egy számot.");
+            int bekert = Int32.Parse(Console.ReadLine());
+            
+            while (bekert !=0 )
+            {
+                szamok.Add(bekert);
+                Console.WriteLine("Adjon meg egy számot. Ha 0 a program leáll.");
+                bekert = Int32.Parse(Console.ReadLine());
+                
+            }
+            szamok.Sort();
+            Console.WriteLine("Legnagyobb szám: {0}", szamok[szamok.Count - 1]);
+
+            Console.ReadKey();
+        }
+    }
+}
