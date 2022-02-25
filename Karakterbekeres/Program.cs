705cb470cff0b343566d92cb212fea8bb05cd192
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-02-25 12:25:30 +0100

    orai feladat

---
+++ Karakterbekeres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karakterbekeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static int[] Osszeg(int x)
+        {
+
+        }
+    }
+}
