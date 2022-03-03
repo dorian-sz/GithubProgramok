5dcfe3111835ad315fbb7e43ed3ad802b06fc1ec
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-03-03 10:00:51 +0100

    primek + objektum

---
+++ inheritance/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class Allat
+    {
+        string nev;
+        string fajta;
+        string szin;
+        int kor;
+        string hang;
+
+        public Allat(string nev, string fajta, string szin)
+        {
+
+            this.nev = nev;
+            this.fajta = fajta;
+            this.szin = szin;
+
+        }
+        public Allat()
+        {
+            this.nev = "Állat";
+            this.fajta = "Valami";
+            this.szin = "Fekete";
+            this.kor = 0;
+        }
+
+        public string GetNev()
+        {
+            return this.nev;
+        }
+        public string GetFajta()
+        {
+            return this.fajta;
+        }
+        public string GetSzin()
+        {
+            return this.szin;
+        }
+        public int GetKor()
+        {
+            return this.kor;
+        }
+
+        public void SetKor(int p)
+        {
+            if (p > 0 && p < 50)
+            {
+                this.kor = p;
+            }
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ primszam/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primszam
+{
+    class Program
+    {
+        static List<int> Prim(int x, List<int> Primek)
+        {
+            int counter = 0;
+            if (Primek.Count <= 499)
+            {
+                for (int i = 1; i < x + 1; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        counter++;
+                    }
+                }
+                if (counter == 2)
+                {
+                    Primek.Add(x);
+                }
+
+                return Prim(x + 1, Primek);
+
+            }
+
+            return Primek;
+
+            
+        }
+        static void Main(string[] args)
+        {
+            List<int> Primek = new List<int>();
+            Console.WriteLine(Prim(2, Primek).Sum());
+
+
+            Console.ReadKey();
+        }
+    }
+}
