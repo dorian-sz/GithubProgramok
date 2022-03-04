0ffb9c2a53766bd1bba7510c9adac283493e2521
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-03-04 11:40:51 +0100

    kor class

---
+++ wat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(12 / 10);
+
+            Console.ReadKey();
+        }
+    }
+}
