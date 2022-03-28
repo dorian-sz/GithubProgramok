0b66ae3a09313e48761c1510b020be17596128df
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-03-28 10:47:45 +0200

    03.28

---
+++ TryCatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            while (repeat)
+            {
+                try
+                {
+                    int num = Int32.Parse(Console.ReadLine());
+                    repeat = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Számot adj meg.");
+                    repeat = true;
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
