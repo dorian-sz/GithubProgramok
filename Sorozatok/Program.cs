45761808c2e2caf5e38314e6d843b76e42d71f01
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-03-31 09:59:53 +0200

    03.31

---
+++ Sorozatok/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorozatok
+{
+    class Program
+    {
+        static string Sorozat(double[] szamok)
+        {
+            double d = szamok[1] - szamok[0];
+            int c = 0;
+
+            for (int i = 0; i < szamok.Length - 1; i++)
+            {
+                if (szamok[i+1] - szamok[i] == d)
+                {
+                    c++;
+                }
+            }
+            if (c == szamok.Length-1)
+            {
+                return "Ez egy számtani sorozat.";
+            }
+            else
+            {
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            double[] sorozat = { 1, 0, -1, -2 };
+            Console.WriteLine(Sorozat(sorozat));
+
+            Console.ReadKey();
+        }
+    }
+}
