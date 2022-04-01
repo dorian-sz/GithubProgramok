86620c99fc39519e2f7326c757181dc7f274a4ff
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-01 09:38:01 +0200

    Binaris

---
+++ Binaris/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binaris
+{
+    class BinarisSzam
+    {
+        private string binaris;
+
+        public BinarisSzam(string binaris)
+        {
+            this.binaris = binaris;
+        }
+
+        public int BinarisCount()
+        {
+            return binaris.Count(f => f == '1');
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarisSzam binary = new BinarisSzam(Console.ReadLine());
+
+            Console.WriteLine(binary.BinarisCount());
+
+            Console.ReadKey();
+        }
+    }
+}
