be3e15205feb16556584a9bbaf7ef8bbec809ce7
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-01 09:25:40 +0200

    Lotto

---
+++ Lotto/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Lotto
+    {
+        private List<int> lottoSzamok;
+
+        public Lotto()
+        {
+            this.lottoSzamok = new List<int>();
+        }
+
+        public void LottoSzamok()
+        {
+            Random rnd = new Random();
+            while (this.lottoSzamok.Count != 5)
+            {
+                int x = rnd.Next(1, 91);
+                if (!this.lottoSzamok.Contains(x))
+                {
+                    this.lottoSzamok.Add(x);
+                }
+            }
+        }
+
+        public List<int> getLottoSzamok()
+        {
+            return this.lottoSzamok;
+        }
+    }
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Lotto lotto = new Lotto();
+
+            lotto.LottoSzamok();
+
+            foreach (var item in lotto.getLottoSzamok())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
