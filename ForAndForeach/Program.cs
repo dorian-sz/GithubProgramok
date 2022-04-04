076c6ff50601ae861bf9d0610622f36113c3e519
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-04 13:23:53 +0200

    ForAndForeach kesz

--- ForAndForeach/Program.cs
+++ ForAndForeach/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace ForAndForeach
 {
+    class ForAndForeach
+    {
+        private List<int> Szamok;
+        Random rnd;
+
+        public ForAndForeach()
+        {
+            Szamok = new List<int>();
+            rnd = new Random();
+        }
+
+        public void Feltolt()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                this.Szamok.Add(this.rnd.Next(10, 100));
+            }
+        }
+
+        public void ForKiir()
+        {
+            for (int i = 0; i < this.Szamok.Count; i++)
+            {
+                Console.WriteLine("For kiiras: {0}", this.Szamok[i]);
+            }
+        }
+
+        public void ForeachKiir()
+        {
+            foreach (var item in this.Szamok)
+            {
+                Console.WriteLine("Foreach kiiras: {0}", item);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            ForAndForeach feladat = new ForAndForeach();
+
+            feladat.Feltolt();
+            feladat.ForKiir();
+            feladat.ForeachKiir();
+
+            Console.ReadKey();
         }
     }
 }
