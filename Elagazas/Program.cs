378289d226aae7fc91b2fb0780191a98da9c473d
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-04 13:12:29 +0200

    Elagazas Kesz

---
+++ Elagazas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elagazas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Szamok = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                Szamok.Add(rnd.Next(10, 100));
+            }
+
+            foreach (var item in Szamok)
+            {
+                if (item%2 == 0)
+                {
+                    Console.WriteLine("Szám: {0}", item);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
