ab2f2f949818a9cd8b003167687f9129b19edd6e
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-04 13:10:29 +0200

    Listak Kesz

---
+++ Listak/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Szamok = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 15; i++)
+            {
+                Szamok.Add(rnd.Next(10, 100));
+            }
+
+            foreach (var item in Szamok)
+            {
+                Console.WriteLine("Szám: {0}", item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
