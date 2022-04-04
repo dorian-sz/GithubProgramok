5eaec2f1410a3dc2e8fe24a4604ab1b49d8d6cdb
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-04 13:19:44 +0200

    Kesz

--- Listak/Program.cs
+++ Listak/Program.cs
@@ -6,21 +6,41 @@ using System.Threading.Tasks;
 
 namespace Listak
 {
+    class Listak
+    {
+        List<int> Szamok;
+        Random rnd;
+
+        public Listak()
+        {
+            Szamok = new List<int>();
+            rnd = new Random();
+        }
+
+        public void Feltolt()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                this.Szamok.Add(rnd.Next(10, 100));
+            }
+        }
+
+        public void Kiir()
+        {
+            foreach (var item in this.Szamok)
+            {
+                Console.WriteLine("Szám: {0}", item);
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            List<int> Szamok = new List<int>();
-            Random rnd = new Random();
-            for (int i = 0; i < 15; i++)
-            {
-                Szamok.Add(rnd.Next(10, 100));
-            }
+            Listak elagazas = new Listak();
 
-            foreach (var item in Szamok)
-            {
-                Console.WriteLine("Szám: {0}", item);
-            }
+            elagazas.Feltolt();
+            elagazas.Kiir();
 
             Console.ReadKey();
         }
