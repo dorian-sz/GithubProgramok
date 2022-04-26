b51c32e0c463eea059cb8a4dc5efe65185f0911d
Dorian Szatmari <dorian.szatmari@outlook.com>
2022-04-26 16:08:17 +0200

    04.26

---
+++ 3d_tomb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3d_tomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] tomb = new int[10, 10, 2];
+
+            tomb[0, 0, 0] = 1;
+            tomb[0, 0, 1] = 2;
+
+            for (int i = 0; i < tomb.GetLength(0); i++)
+            {
+               
+                for (int j = 0; j < tomb.GetLength(1); j++)
+                {
+                    for (int k = 0; k < tomb.GetLength(2); k++)
+                    {
+                        Console.Write("{0} ",tomb[i, j, k]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
